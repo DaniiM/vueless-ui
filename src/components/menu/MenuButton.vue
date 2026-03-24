2e241476be3436e13fc6f2aa2cdbcfa95dc1a052
Danijel Marojević <danijelmarojevicglibo99@gmail.com>
2026-03-24 13:20:13 +0100

    Initial commit for Vueless UI

---
+++ src/components/menu/MenuButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { inject } from 'vue'
+
+const menu = inject('menu')
+
+function handleClick() {
+  menu.toggle()
+}
+</script>
+
+<template>
+  <button @click="handleClick">
+    <slot />
+  </button>
+</template>
---
+++ src/components/menu/MenuItems.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { inject } from 'vue'
+
+const menu = inject('menu')
+</script>
+
+<template>
+  <div v-if="menu.isOpen">
+    <slot />
+  </div>
+</template>
