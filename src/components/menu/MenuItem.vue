08eacfdd073e2f75e9fa4a818ab29e24cac43ff7
Danijel Marojević <danijelmarojevicglibo99@gmail.com>
2026-03-24 14:29:35 +0100

    - Added Menu, MenuButton, MenuItems, MenuItem - Implemented open/close logic - Added keyboard navigation (ArrowUp, ArrowDown, Enter, Space, Esc) - Added scoped CSS for basic styling - Created utils/keyboard.js and utils/dom.js for reusable helpers

--- src/components/menu/MenuItem.vue
+++ src/components/menu/MenuItem.vue
@@ -1,8 +1,38 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+  registerItem: Function
+})
+
+const el = ref(null)
+
+onMounted(() => {
+  if (props.registerItem) props.registerItem(el.value)
+})
 </script>
 
 <template>
-  <div role="menuitem">
+  <div
+    ref="el"
+    class="menu-item"
+    role="menuitem"
+    tabindex="-1"
+  >
     <slot />
   </div>
 </template>
+
+<style scoped>
+.menu-item {
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.menu-item:hover,
+.menu-item:focus {
+  background-color: #f3f4f6;
+  outline: none;
+}
+</style>
