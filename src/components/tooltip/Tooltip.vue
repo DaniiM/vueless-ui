f02200125272aafeee236621aa43c8537408d86e
Danijel Marojević <danijelmarojevicglibo99@gmail.com>
2026-03-28 18:43:31 +0100

    feat: added basic tooltip logic

---
+++ src/components/tooltip/Tooltip.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, provide } from 'vue'
+import { TOOLTIP_CTX } from './context'
+
+const isOpen = ref(false)
+const triggerRef = ref(null)
+const contentRef = ref(null)
+
+function open() {
+    isOpen.value = true
+}
+
+function close() {
+    isOpen.value = false
+}
+
+provide(TOOLTIP_CTX, {
+  isOpen,
+  open,
+  close,
+  triggerRef,
+  contentRef
+})
+</script>
+
+<template>
+    <slot/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/tooltip/TooltipTrigger.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useTooltipContext } from './useTooltipContext'
+
+const { open, close, triggerRef } = useTooltipContext()
+
+const el = ref(null)
+
+onMounted(() => {
+  triggerRef.value = el.value
+})
+</script>
+
+<template>
+    <span
+      ref="el"
+      @mouseenter="open"
+      @mouseleave="close"
+      @focus="open"
+      @blur="close"
+    >
+      <slot />
+    </span>
+  </template>
