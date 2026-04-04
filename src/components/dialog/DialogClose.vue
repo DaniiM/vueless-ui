10b7fdbcd9264a924250065869a89e73834454f2
Danijel Marojević <danijelmarojevic@Danijels-MacBook-Air.local>
2026-04-04 08:54:48 +0200

    feat: dialog buttons styling update, disabled implemented

--- src/components/dialog/DialogClose.vue
+++ src/components/dialog/DialogClose.vue
@@ -12,9 +12,25 @@
 </script>
 
 <template>
-  <component :is="$props.as" type="button" @click="close">
+  <component :is="$props.as" type="button" @click="close" class="close-button">
     <slot />
   </component>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .close-button {
+    background-color: #6b7280;
+    color: white;
+    padding: 0.5rem 1rem;
+    border-radius: 3px;
+    border: none;
+    cursor: pointer;
+    font-size: 0.75rem;
+    transition: background-color 0.2s;
+  }
+
+  .close-button:hover {
+    outline: 2px solid #4d5159;
+    outline-offset: 2px;
+  }
+</style>
--- src/components/dialog/DialogTrigger.vue
+++ src/components/dialog/DialogTrigger.vue
@@ -6,15 +6,68 @@
       type: String,
       default: 'button',
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   });
 
   const { toggle } = useDialogContext();
+
+  function handleClick() {
+    if (props.disabled) return;
+
+    toggle();
+  }
 </script>
 
 <template>
-  <component :is="props.as" type="button" @click="toggle">
+  <component
+    :is="props.as"
+    type="button"
+    @click="handleClick"
+    class="dialog-button"
+    :disabled="props.disabled"
+  >
     <slot />
   </component>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .dialog-button {
+    background-color: #459cf4;
+    color: white;
+    padding: 0.5rem 1rem;
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+    font-size: 1rem;
+    transition: background-color 0.2s;
+    user-select: none;
+    transition: color 0.5s ease;
+  }
+
+  .dialog-button:hover {
+    background-color: #88bdf2;
+  }
+
+  .dialog-button:focus {
+    outline: 2px solid #bdddfc;
+    outline-offset: 2px;
+  }
+
+  .dialog-button:disabled {
+    background-color: #6b7280;
+    cursor: not-allowed;
+  }
+
+  .dialog-button:disabled:hover {
+    background-color: #384959;
+    cursor: not-allowed;
+  }
+
+  .dialog-button:disabled:focus {
+    background-color: #384959;
+    cursor: not-allowed;
+  }
+</style>
